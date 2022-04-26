6954ebefeaeb9c227602902b79210ec8becaf8f8
letzgorts <a01030659322@gmail.com>
2022-04-26 11:29:18 +0900

    BMI 계산기 레이아웃

---
+++ BMIcalculator/app/src/main/java/learning/kotlin/part2/bmicalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package learning.kotlin.part2.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
