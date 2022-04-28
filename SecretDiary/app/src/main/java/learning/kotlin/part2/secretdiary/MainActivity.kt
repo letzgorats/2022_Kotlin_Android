af682acfc6c2a967d05b4b9cd33b19ceebe28053
letzgorts <a01030659322@gmail.com>
2022-04-29 01:36:08 +0900

    비밀 다이어리

---
+++ SecretDiary/app/src/main/java/learning/kotlin/part2/secretdiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package learning.kotlin.part2.secretdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
